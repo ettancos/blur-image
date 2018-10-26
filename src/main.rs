dd1c8fbed29989e74d91e6ae3fa0054e828caf61
Tamás Táncos <tamas.tancos@emarsys.com>
2018-10-26 10:37:47 +0200

    Add bit of error handling

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,22 @@
 extern crate clap;
 extern crate image;
 
+use clap::{App, AppSettings, Arg};
+use image::{DynamicImage, GenericImageView, ImageResult};
 use std::path::{Path, PathBuf};
-use clap::{Arg, App, AppSettings};
-use image::{ImageResult, DynamicImage, GenericImageView};
 
-fn read_file(filename: &Path) -> DynamicImage {
-    return image::open(filename).expect("something went wrong reading the file");
+fn read_file(filename: &Path) -> ImageResult<DynamicImage> {
+    image::open(filename)
 }
 
 fn save_file(path: &Path, img: DynamicImage) -> Result<(), std::io::Error> {
-    image::save_buffer(path, &img.raw_pixels(), img.width(), img.height(), img.color())
+    image::save_buffer(
+        path,
+        &img.raw_pixels(),
+        img.width(),
+        img.height(),
+        img.color(),
+    )
 }
 
 fn parse_args() -> (PathBuf, PathBuf, f32) {
@@ -37,16 +43,42 @@ fn parse_args() -> (PathBuf, PathBuf, f32) {
 
     let input: String = value_t_or_exit!(matches.value_of("image"), String);
     let input_path = PathBuf::from(input.as_str());
-    let default_output = format!("{}_blurred.{}", input_path.file_stem().unwrap().to_str().unwrap(), input_path.extension().unwrap().to_str().unwrap());
-    let output: String = value_t!(matches.value_of("output"), String).unwrap_or(String::from(default_output.as_str()));
+
+    // TODO: parent to have full path
+    let default_output = format!(
+        "{}_blurred.{}",
+        input_path.file_stem().unwrap().to_str().unwrap(),
+        input_path.extension().unwrap().to_str().unwrap()
+    );
+
+    let output: String = value_t!(matches.value_of("output"), String)
+        .unwrap_or(String::from(default_output.as_str()));
+
     let sigma = value_t_or_exit!(matches.value_of("sigma"), f32);
+
     return (input_path, PathBuf::from(output.as_str()), sigma);
 }
 
 fn main() {
     let (input, output, sigma) = parse_args();
+
     println!("Reading image {:?}", input.to_str().unwrap());
-    let img: DynamicImage = read_file(input.as_path());
-    save_file(output.as_path(), img.blur(sigma));
-    println!("Saved blurred ({:?}) image to {:?}", sigma, output.to_str().unwrap());
+    let img: DynamicImage = match read_file(input.as_path()) {
+        Result::Ok(i) => i,
+        Result::Err(err) => {
+            println!("something went wrong reading the file {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    match save_file(output.as_path(), img.blur(sigma)) {
+        Result::Ok(_) => println!("Saved blurred ({:?}) image to {:?}", sigma, output),
+        Result::Err(err) => println!("Error: {:?}", err),
+    }
+
+    println!(
+        "Saved blurred ({:?}) image to {:?}",
+        sigma,
+        output.to_str().unwrap()
+    );
 }
